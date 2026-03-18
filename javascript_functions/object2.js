86728a99c46122f51f06e4fc103b8256a79ecefa
Ritik Sawarkar <ritiksawarkar027@gmail.com>
2026-03-18 16:36:05 +0530

    object

---
+++ javascript_functions/object2.js
@@ -0,0 +1,7 @@
+const obj = {
+  name: "Ritik Sawarkar",
+  age: "20",
+  gender: "male",
+};
+obj.age = 26;
+console.log(obj["age"]);
---
+++ javascript_functions/object4.js
@@ -0,0 +1,8 @@
+const data = {
+  name: "Ritik Sawarkar",
+  func: () => {
+    console.log("Hello World");
+  },
+};
+
+data.func();
